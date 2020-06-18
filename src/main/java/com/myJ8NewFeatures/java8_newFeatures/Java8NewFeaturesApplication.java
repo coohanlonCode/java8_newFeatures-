33def453f57cf3cae25490996d44a139b6d39961
Colin O'Hanlon <coohanlonCode@google.com>
2020-06-18 12:11:55 -0400

    Initial commit

---
+++ src/main/java/com/myJ8NewFeatures/java8_newFeatures/Java8NewFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.myJ8NewFeatures.java8_newFeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8NewFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8NewFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myJ8NewFeatures/java8_newFeatures/Java8NewFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myJ8NewFeatures.java8_newFeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8NewFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
